051a082d13787e35588ddb6c44eb14c897bcff8b
amal_jith <123450863+amal-jith@users.noreply.github.com>
2025-04-16 17:02:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app.debug=True
 def home():
     return render_template("home.html")
 
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/presentation-design")
+def presentation():
+    return render_template("presentation-design.html")
+
+@app.route("/solutions")
+def solutions():
+    return render_template("solutions.html")
+
 
 
 app.run(host='0.0.0.0', port=443)
